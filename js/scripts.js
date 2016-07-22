1eab28eed2bbf4af9c9b6ab9a847cbdb903f4a9e
Zach Holz <zach.a.holz@gmail.com>
2016-07-22 16:11:28 -0700

    Create js constructor for pizza, create html skeleton with checkbox form

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+// business logic
+
+  //pizza object -- may not need
+var yourPizza = {
+  toppings = [],
+  size = "";
+}
+
+  //pizza constructor
+function Pizza(toppings, size) {
+  this.toppings = toppings;
+  this.size = size;
+}
+
+//pizza prototype
+Pizza.prototype.toppingsSum = function(topping) {
+  forEach(yourPizza.toppings)
+}
+
+// user interface logic
+$(document).ready(function() {
+  $("form#pizza-choices").submit(function(event) {
+
+  });
+});
