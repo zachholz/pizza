f98edf591a2107e190400bfae6e97632531d5526
Zach Holz <zach.a.holz@gmail.com>
2016-07-25 19:19:18 -0700

    Define global variables and capture user input.

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,32 @@
 // business logic
+// global variables
 
-  //pizza object -- may not need
-var yourPizza = {
-  toppings = [],
-  size = "";
-}
+var pizzaCost = 0;
+var size = "";
+var cheese = "";
+var toppings = [],
 
-  //pizza constructor
-function Pizza(toppings, size) {
-  this.toppings = toppings;
+//pizza constructor
+function Pizza(size, cheese, toppings) {
   this.size = size;
+  this.cheese = cheese;
+  this.toppings = toppings;
 }
 
 //pizza prototype
 Pizza.prototype.toppingsSum = function(topping) {
-  forEach(yourPizza.toppings)
+  yourPizza.topppings.forEach(function(topping) {
+    return pizzaCost +=
+  })
 }
 
 // user interface logic
 $(document).ready(function() {
   $("form#pizza-choices").submit(function(event) {
-
+    size = $("input:radio[name=size]:checked").val();
+    cheese = $("input:radio[name=cheese]:checked").val();
+    toppings = $("input:checkbox:checked").map(function() {
+      return this.value;
+    }).get();
   });
 });
